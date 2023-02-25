50ec8df8e4591e5d929ee97c39e75b57d5bd02b3
Ekaterina <katerina331-441@ya.ru>
2023-02-25 20:48:54 +0300

    Commit new Branch Lombok

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,63 +1,70 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     int maxRadioStation = 10;
     int numberRadioStation;
     int maxVolumeRadio = 100;
     int volumeRadio;
-
-    public Radio (int maxRadioStation) {
-        this.maxRadioStation = maxRadioStation;
-    }
-
-    public Radio () {
-    }
-
-    public int station() {
-        return numberRadioStation;
-    }
-    public int maxStation() {
-        return maxRadioStation;
-    }
-
-    public int volume() {
-        return volumeRadio;
-    }
-
-    public void nextStation() {
-        if (numberRadioStation == maxRadioStation-1) {
-            numberRadioStation = 0;
-            return;
-        }
-        numberRadioStation++;
-    }
-
-    public void prevStation() {
-        if (numberRadioStation == 0) {
-            numberRadioStation = maxRadioStation-1;
-            return;
-        }
-        numberRadioStation--;
-    }
-
-    public void numStation(int numberStation) {
-        if (numberStation < 0) {
-            return;
-        }
-        if (numberStation > maxRadioStation-1) {
-            return;
-        }
-        numberRadioStation = numberStation;
-    }
-
-    public void upVolume() {
-        if (volumeRadio < maxVolumeRadio) {
-            volumeRadio++;
-        }
-    }
-
-    public void downVolume() {
-        if (volumeRadio > 0) {
-            volumeRadio--;
-        }
-    }
 }
+//
+//    public Radio (int maxRadioStation) {
+//        this.maxRadioStation = maxRadioStation;
+//    }
+//
+//    public Radio () {
+//    }
+//
+//    public int station() {
+//        return numberRadioStation;
+//    }
+//    public int maxStation() {
+//        return maxRadioStation;
+//    }
+//
+//    public int volume() {
+//        return volumeRadio;
+//    }
+//
+//    public void nextStation() {
+//        if (numberRadioStation == maxRadioStation-1) {
+//            numberRadioStation = 0;
+//            return;
+//        }
+//        numberRadioStation++;
+//    }
+//
+//    public void prevStation() {
+//        if (numberRadioStation == 0) {
+//            numberRadioStation = maxRadioStation-1;
+//            return;
+//        }
+//        numberRadioStation--;
+//    }
+//
+//    public void numStation(int numberStation) {
+//        if (numberStation < 0) {
+//            return;
+//        }
+//        if (numberStation > maxRadioStation-1) {
+//            return;
+//        }
+//        numberRadioStation = numberStation;
+//    }
+//
+//    public void upVolume() {
+//        if (volumeRadio < maxVolumeRadio) {
+//            volumeRadio++;
+//        }
+//    }
+//
+//    public void downVolume() {
+//        if (volumeRadio > 0) {
+//            volumeRadio--;
+//        }
+//    }
+//}
